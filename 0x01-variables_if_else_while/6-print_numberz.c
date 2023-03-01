d7d8fec03c0ac286eb01d9cc5927584a0a138c83
jebiwott <jebiwottbelindah@gmail.com>
2023-03-01 09:59:07 -0800

    4-print_alphabt.c,5-print_numbers.c,6-print_numberz.c,7-print_tebahpla.c,8-print_base16.c, 9-print_comb.c,100-print_comb3.c,101-print_comb4.c,102-print_comb5.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - No Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
